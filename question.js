f55a7bd6ad828713a882c98656e2ede9767d899d
Stephanie McSpedon-Marx <s.mcspedon@gmail.com>
2017-11-15 14:55:38 +0000

    automatically backed up by learn

--- question.js
+++ question.js
@@ -7,12 +7,16 @@ class Question {
     this.save()
    }
 
+   static All(){
+     return this._All;
+   }
+
   save () {
     this.constructor._All.push(this)
    }
 
   static Find(id){
     return this._All[id-1]
-   } 
+   }
 
 }
